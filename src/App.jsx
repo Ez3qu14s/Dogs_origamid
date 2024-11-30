cfb8172a2c25b5f6214d16934565e7228092d9bd
Ezequias Bernardo <ezequiasobernardo@outlook.com>
2024-11-30 19:45:35 -0300

    feat: estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/';
+import Footer from './Components/Footer/';
+import Home from './Components/Home';
+import Login from './Components/Login';
+
+import './App.css';
 
 function App() {
   return (
     <>
-      <div>App React</div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
