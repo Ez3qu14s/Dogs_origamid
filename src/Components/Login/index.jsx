4dff4568db023a42297219b582bc718961244fc7
Ezequias Bernardo <ezequiasobernardo@outlook.com>
2024-11-30 22:21:41 -0300

    feat: inicio dos formularios

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LogiPasswordLost from './LoginPasswordLost';
+import LogiPasswordReset from './LoginPasswordLost';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LogiPasswordLost />} />
+        <Route path="perdeu" element={<LogiPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
