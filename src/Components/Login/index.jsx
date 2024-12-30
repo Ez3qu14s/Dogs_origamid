95b81e67df6c4192a88e218a6f2426622038351e
Ezequias Bernardo <ezequiasobernardo@outlook.com>
2024-12-30 17:24:33 -0300

    feat: finalizacao do contexto

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LogiPasswordLost from './LoginPasswordLost';
 import LogiPasswordReset from './LoginPasswordLost';
+import { UserContext } from '../../UserContext';
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
+
   return (
     <div>
       <Routes>
