ff45f28964583e0a9b5dca194e2c29e4b4b38827
Ezequias Bernardo <ezequiasobernardo@outlook.com>
2025-01-01 20:28:46 -0300

    feat: login de criar

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -6,20 +6,24 @@ import LogiPasswordLost from './LoginPasswordLost';
 import LogiPasswordReset from './LoginPasswordLost';
 import { UserContext } from '../../UserContext';
 
+import styles from './Login.module.css';
+
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LogiPasswordLost />} />
-        <Route path="perdeu" element={<LogiPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LogiPasswordLost />} />
+          <Route path="perdeu" element={<LogiPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
